8fc7e9123159a8d9eee36c0c2aa21c7171c1f67a
freddymartinezfm1 <38080761+freddymartinezfm1@users.noreply.github.com>
2020-02-06 17:20:46 -0600

    changes

--- Log.h
+++ Log.h
@@ -9,5 +9,14 @@ public:
 	static void m(std::string, std::string, std::string);	
 };
 
+void Log::m(std::string TAG, std::string mTAG){
+	m(TAG, mTAG, " ");
+	//std::cout << TAG << ": " << mTAG  << std::endl;
+}
+
+void Log::m(std::string TAG, std::string mTAG, std::string message){
+	std::cout << TAG << ": " << mTAG << " " << message  << std::endl;
+}
+
 
 
